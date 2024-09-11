5a49b9f020bb26dd90322542689718297e887be2
an <an@andeMacBook-Pro.local>
2024-09-11 22:15:27 +0800

    first kernel

--- 1.kernel/test1.cu
+++ 1.kernel/test1.cu
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+__global__ void my_first_kernel()
+{
+    printf("hello world from GPU");
+
+}
+
 int main()
 {
+
     printf("hello world form cpu \n");
+    my_first_kernel();
+    
     return 0;
 }
